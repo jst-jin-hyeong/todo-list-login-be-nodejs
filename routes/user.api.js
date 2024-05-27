374b6a69615be9791e5e23ecd21decd85a20b769
jhp <daramjw@naver.com>
2024-05-27 13:51:38 +0900

    갑자기 login router가 먹히기 시작했다. user.api.js - router.post까지 동작 확인

--- routes/user.api.js
+++ routes/user.api.js
@@ -2,21 +2,21 @@ const express = require("express");
 const taskController = require("../controller/task.controller");
 const router = express.Router();
 
-// //1. 회원가입 endpoint
-// router.post("/", (req, res) => {
-//   console.log("post 여기까지 타고는 들어오나");
-//   res.send("post 되나");
-// });
+//1. 회원가입 endpoint
+router.post("/", (req, res) => {
+  console.log("post 여기까지 타고는 들어오나");
+  res.send("post 되나");
+});
 
-// router.get("/", taskController.getTask);
+router.get("/", taskController.getTask);
 
-// module.exports = router;
+module.exports = router;
 
-// //
+//
 
 router.post("/", taskController.createTask);
 
-router.get("/", taskController.getTask);
+// router.get("/", taskController.getTask);
 
 router.put("/:id", taskController.updateTask);
 
